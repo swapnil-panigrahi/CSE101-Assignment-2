4c26ef87a317c0503f7efd508d02705165214d98
critz㌹ <pokemonmasterswapnil@gmail.com>
2023-01-10 20:51:54 +0530

    Update Q1

--- Q1.py
+++ Q1.py
@@ -9,14 +9,15 @@ menu = [("Samosa", 15), ("Idli", 30), ("Maggie", 50), ("Chilli Potato", 50), ("D
 
 printmenu()
 order_list=[]
+
+print("Enter your item number and quantity seperated by a space:")
 while True:
-    order=input("Enter your item number and quantity seperated by a space: ").strip()
+    order=input().strip()
     if order=="":
         break
     else:
         order_list.append(list(map(int,order.split())))
 
-print()
 for i in order_list:
     if 0<i[0]-1<10:
         print(menu[i[0]-1][0]+" "*(15-len(menu[i[0]-1][0]))+str(i[1]),"Rs",str(menu[i[0]-1][1]*i[1]))
